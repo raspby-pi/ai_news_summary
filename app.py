97d07cfea5e5d8817222b263c1d03af6363039bc
raspby-pi <jsbshorts12302@gmail.com>
2026-02-01 16:48:37 +0900

    구글 서치 콘솔

--- app.py
+++ app.py
@@ -23,12 +23,11 @@ st.set_page_config(
 
 components.html(
     """
-    <script>
-        var meta = document.createElement('meta');
-        meta.name = "google-site-verification";
-        meta.content = "bsw-b6GuWL7Lt2xAUa1pFlSF65xYdoxBJHlIP6JgQ6Y";
-        document.getElementsByTagName('head')[0].appendChild(meta);
-    </script>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TV88SBZR');</script>
     """,
     height=0,
 )
